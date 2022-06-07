cb2cf88bcb370506b89dc1f5eb253c6cc0371427
Sylek <sylek38@gmail.com>
2022-06-08 00:22:12 +0200

    Initial setup with PWA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
+import React from "react";
 import "./App.css";
-import { ExampleComponent } from "./ExampleComponent";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <ExampleComponent>Children</ExampleComponent>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>elko</div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
