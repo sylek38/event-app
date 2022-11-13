7b399bb1c04d0a48e285d667a7d3c55e662f8947
bartekmnemo <bartekmnemo@gmail.com>
2022-11-13 18:32:56 +0100

    add slider component

--- frontend/components/header/Header.style.ts
+++ frontend/components/header/Header.style.ts
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import { pxToRem } from "../../utils/pxToRem";
 
 export const Header = styled.header`
+    display: flex;
     background-color: var(--primary-background);
     padding: ${pxToRem(45)} 0 ${pxToRem(45)} ${pxToRem(120)};
 
-    /* > div {
-        height: 50px;
-    } */
+    > div {
+        margin-right: 50px;
+    }
 
     margin: 0 0 ${pxToRem(50)} ${pxToRem(100)};
 `;
--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import * as S from "./Header.style";
 import { Search } from "../../components/inputs/searchbar/Searchbar";
+import { Slider } from "../../components/inputs/slider/Slider";
 
 export const Header = () => {
     return (
@@ -13,6 +14,9 @@ export const Header = () => {
                 // fullWidth
                 // blackVariant
             ></Search>
+
+            <Slider type="range" min={1} max={12} />
+            {/* fullWidth */}
         </S.Header>
     );
 };
