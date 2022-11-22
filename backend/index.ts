e2e82ec4698f84b2f0de9df8b9ee36b393d7fb76
bartekmnemo <bartekmnemo@gmail.com>
2022-11-22 17:59:43 +0100

    add mongoDB

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+const mongoose = require("mongoose");
 
 dotenv.config();
 
@@ -7,6 +8,11 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT;
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(console.log("conntected to mongoDB"))
+	.catch((err: any) => console.log(err));
 
 app.get("/", (req, res) => {
 	res.send("Express + TypeScript Server");
