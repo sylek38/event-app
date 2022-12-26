bce3e774512e817feceadbad36baaf2ba4867042
bartekmnemo <bartekmnemo@gmail.com>
2022-12-26 11:44:45 +0100

    add backend for register

--- frontend/views/register/RegisterView.tsx
+++ frontend/views/register/RegisterView.tsx
@@ -9,27 +9,42 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import useTranslation from "next-translate/useTranslation";
 import { Button } from "../../components/button/Button";
 
+import Link from "next/link";
+import { Routes } from "../../routes/Routes";
+import { useAPISignUp } from "../../api/auth/useAPISignUp";
+
 interface FormTypes {
-    register_name: string;
-    register_lastname: string;
-    register_email: string;
-    register_password: string;
-    select_city: string;
-    search_bar: string;
+    name: string;
+    surname: string;
+    email: string;
+    bio: string;
+    password: string;
+    profilePic: string;
 }
 
 export const RegisterView = () => {
     const { t } = useTranslation("global");
+
+    const { isError, isLoading, mutateAsync } = useAPISignUp();
 
     const {
         register,
         control,
         formState: { errors },
         handleSubmit,
-    } = useForm<FormTypes>();
+    } = useForm<FormTypes>({
+        defaultValues: {
+            name: "",
+            surname: "",
+            email: "",
+            bio: "",
+            password: "",
+            profilePic: "",
+        },
+    });
 
-    const onSubmit: SubmitHandler<FormTypes> = (data) => {
-        console.log(data);
+    const onSubmit: SubmitHandler<FormTypes> = async (data) => {
+        await mutateAsync(data);
     };
 
     return (
@@ -38,34 +53,34 @@ export const RegisterView = () => {
                 <S.Form onSubmit={handleSubmit(onSubmit)}>
                     <S.Header>{t("sign_up_title")}</S.Header>
                     <TextInput
-                        id="register_name"
+                        id="name"
                         register={register}
                         control={control}
-                        isError={!!errors.register_name}
+                        isError={!!errors.name}
                         required
                         dark
                     />
                     <TextInput
-                        id="register_lastname"
+                        id="surname"
                         register={register}
                         control={control}
-                        isError={!!errors.register_lastname}
+                        isError={!!errors.surname}
                         required
                         dark
                     />
                     <TextInput
-                        id="register_email"
+                        id="email"
                         register={register}
                         control={control}
-                        isError={!!errors.register_email}
+                        isError={!!errors.email}
                         required
                         dark
                     />
                     <TextInput
-                        id="register_password"
+                        id="password"
                         register={register}
                         control={control}
-                        isError={!!errors.register_password}
+                        isError={!!errors.password}
                         type="password"
                         required
                         dark
@@ -95,10 +110,15 @@ export const RegisterView = () => {
                     </S.GoogleButton>
                     <S.Footer>
                         <S.FooterSpan>{t("have_account")}</S.FooterSpan>
-                        <S.FooterHref>{t("sign_in")}</S.FooterHref>
+                        <Link href={Routes.LOGIN}>
+                            <S.FooterHref>{t("sign_in")}</S.FooterHref>
+                        </Link>
                     </S.Footer>
                 </S.Form>
             </S.RightSide>
+            {isError && <div>isError</div>}
+
+            {isLoading && <div>isLoading</div>}
             <S.LeftSide></S.LeftSide>
         </S.Container>
     );
