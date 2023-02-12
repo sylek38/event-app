1d76131a1823bbbb2ff02e6d937a91d2f375b68e
Sylek <sylek38@gmail.com>
2023-02-12 23:06:24 +0100

    Add config title to main page

--- frontend/config.ts
+++ frontend/config.ts
@@ -3,6 +3,7 @@ export const DEFAULT_EMAIL = "default@gmail.com";
 export const DEFAULT_PASS = "default1";
 export const AUTH_COOKIE = "token";
 export const AUTH_REFRESH_COOKIE = "refreshToken";
+export const CONFIG_TITLE = "Eventik";
 
 // PAGINATION DEFAULTS
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,34 @@
 import type { GetServerSideProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
+import useTranslation from "next-translate/useTranslation";
 import { fetchAPIAuth } from "../api/auth/useAPIAuth";
+import { CONFIG_TITLE } from "../config";
 import { LoginView } from "../views/login/LoginView";
 
-const Home: NextPage = () => <LoginView />;
+const Home: NextPage = () => {
+    const { t } = useTranslation("global");
+
+    return (
+        <>
+            <NextSeo title={`${t("sign_in_title")} - ${CONFIG_TITLE}`} />
+            <LoginView />
+        </>
+    );
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const csrf = req.cookies["csrf"] ?? "";
 
     try {
         const auth = await fetchAPIAuth({ csrf });
-        // if (auth) {
-        //     return {
-        //         redirect: {
-        //             destination: "/events",
-        //             permanent: false,
-        //         },
-        //     };
-        // }
+        if (auth) {
+            return {
+                redirect: {
+                    destination: "/events",
+                    permanent: false,
+                },
+            };
+        }
     } catch (err) {
         delete req.cookies["csrf"];
         console.log(err);
